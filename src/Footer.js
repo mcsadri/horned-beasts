68ad6af67f1c7e7532a667522dfaa2d73fbff50f
Manuch Sadri <colonquebang@hotmail.com>
2022-09-17 22:06:23 -0700

    completed base component requirements

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render () {
+        return (
+            <>
+                <p>Author: Manuch Sadri</p>
+            </>
+        )
+    }
+}
+
+export default Footer;
