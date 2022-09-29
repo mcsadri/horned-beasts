1d691dad7e77d27bf33dbf80bd23a8106adb4a1f
Manuch Sadri <colonquebang@hotmail.com>
2022-09-28 22:45:35 -0700

    completed Lab 04 filter feature

--- src/App.js
+++ src/App.js
@@ -12,30 +12,39 @@ class App extends React.Component {
     super(props);
     this.state = {
       show: false,
-      selectedBeast: {}
+      selectedBeast: {},
+      horns: -1,
+      beasts: data,
     }
   }
 
   handleClose = () => this.setState({show: false});
 
-  handleOpen = (beastId) => {
+  handleOpen = beastId => {
     console.log('beastId: ', beastId);
     const selectedBeast = data.find(beast => beast._id === beastId);
     this.setState({show: true, selectedBeast: selectedBeast});
   }
 
+  selectHorns = event => {
+    event.preventDefault();
+    this.setState({horns: Number(event.target.value)}, () => this.state.horns === -1 ? this.setState({beasts: data}) : this.setState({beasts: data.filter(currentBeast => currentBeast.horns === this.state.horns)}));
+  }
+
 render() {
     return (
       <div className="App">
-        <Header />
+        <Header 
+          selectHorns = {this.selectHorns}
+        />
         <Main
-          beasts = {data}
-          handleOpen={this.handleOpen}
+          beasts = {this.state.beasts}
+          handleOpen = {this.handleOpen}
         />
         <BeastModal
-          show={this.state.show}
-          handleClose={this.handleClose}
-          selectedBeast={this.state.selectedBeast}
+          show = {this.state.show}
+          handleClose = {this.handleClose}
+          selectedBeast = {this.state.selectedBeast}
         />
         <Footer />
       </div>
