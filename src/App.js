9281562e456f36f46a2472f2f5e03b58e4032d36
Manuch Sadri <colonquebang@hotmail.com>
2022-10-14 08:51:25 -0700

    syncing repos

--- src/App.js
+++ src/App.js
@@ -8,48 +8,48 @@ import BeastModal from './BeastModal';
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false,
-      selectedBeast: {},
-      horns: -1,
-      beasts: data,
+    constructor(props) {
+        super(props);
+        this.state = {
+            show: false,
+            selectedBeast: {},
+            horns: -1,
+            beasts: data,
+        }
     }
-  }
 
-  handleClose = () => this.setState({show: false});
+    handleClose = () => this.setState({ show: false });
 
-  handleOpen = beastId => {
-    console.log('beastId: ', beastId);
-    const selectedBeast = data.find(beast => beast._id === beastId);
-    this.setState({show: true, selectedBeast: selectedBeast});
-  }
+    handleOpen = beastId => {
+        console.log('beastId: ', beastId);
+        const selectedBeast = data.find(beast => beast._id === beastId);
+        this.setState({ show: true, selectedBeast: selectedBeast });
+    }
 
-  selectHorns = event => {
-    event.preventDefault();
-    this.setState({horns: Number(event.target.value)}, () => this.state.horns === -1 ? this.setState({beasts: data}) : this.setState({beasts: data.filter(currentBeast => currentBeast.horns === this.state.horns)}));
-  }
+    selectHorns = event => {
+        event.preventDefault();
+        this.setState({ horns: Number(event.target.value) }, () => this.state.horns === -1 ? this.setState({ beasts: data }) : this.setState({ beasts: data.filter(currentBeast => currentBeast.horns === this.state.horns) }));
+    }
 
-render() {
-    return (
-      <div className="App">
-        <Header 
-          selectHorns = {this.selectHorns}
-        />
-        <Main
-          beasts = {this.state.beasts}
-          handleOpen = {this.handleOpen}
-        />
-        <BeastModal
-          show = {this.state.show}
-          handleClose = {this.handleClose}
-          selectedBeast = {this.state.selectedBeast}
-        />
-        <Footer />
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Header
+                    selectHorns={this.selectHorns}
+                />
+                <Main
+                    beasts={this.state.beasts}
+                    handleOpen={this.handleOpen}
+                />
+                <BeastModal
+                    show={this.state.show}
+                    handleClose={this.handleClose}
+                    selectedBeast={this.state.selectedBeast}
+                />
+                <Footer />
+            </div>
+        );
+    }
 }
 
 export default App;
